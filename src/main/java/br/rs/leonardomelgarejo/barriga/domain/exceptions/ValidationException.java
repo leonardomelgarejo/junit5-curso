dd855d5dadcd41c1c854b2e61736ba1e92f4ec75
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-01-17 11:39:46 +0000

    Teste inclusão usuário válido

---
+++ src/main/java/br/rs/leonardomelgarejo/barriga/domain/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package br.rs.leonardomelgarejo.barriga.domain.exceptions;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/UsuarioTest.java
@@ -0,0 +1,18 @@
+import br.rs.leonardomelgarejo.barriga.domain.Usuario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Domínio: Usuário")
+public class UsuarioTest {
+
+    @Test
+    @DisplayName("Deve criar um usuário válido")
+    public void deveCriarUsuarioValido(){
+        Usuario usuario = new Usuario(1L, "Usuario Valido", "user@mail.com", "123456");
+        Assertions.assertEquals(1L, usuario.id());
+        Assertions.assertEquals("Usuario Valido", usuario.nome());
+        Assertions.assertEquals("user@mail.com", usuario.email());
+        Assertions.assertEquals("123456", usuario.senha());
+    }
+}
